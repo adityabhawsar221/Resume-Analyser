fa1e08865129d31cd5925ae635637e0fe9afa672
Aditya Bhawsar <adityabhawsar21@gmail.com>
2025-08-22 19:26:22 +0530

    Change background image format from SVG to JPG

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -91,7 +91,7 @@ const Upload = () => {
     }
 
     return (
-        <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <main className="bg-[url('/images/bg-main.jpg')] bg-cover">
             <Navbar />
 
             <section className="main-section">
